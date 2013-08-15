2ba65946dfd062cf311d6549ae05921ccbbb80f4
Mattias Linnap <mattias@linnap.com>
2013-08-15 15:20:04 +0100

    Password protection, deleting uploads.

--- latencyping/urls.py
+++ latencyping/urls.py
@@ -1,11 +1,17 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'ping.views.index'),
     url(r'^ping/([0-9]+)/$', 'ping.views.ping'),
     url(r'^upload/([^/]+)/$', 'ping.views.upload'),
+    url(r'^delete/([0-9]+)/$', 'ping.views.delete'),
+
+    #url('^accounts/', include('django.contrib.auth.urls')),
+    url('^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
--- ping/views.py
+++ ping/views.py
@@ -1,12 +1,14 @@
 
 from collections import defaultdict
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.csrf import csrf_exempt
 
 from ping.models import Upload
 
 
+@login_required
 def index(request):
     uploads = list(Upload.objects.order_by('time').all())
     byname = defaultdict(list)
@@ -17,10 +19,17 @@ def index(request):
     return render(request, 'ping/index.html', {'byname': byname})
 
 
+@login_required
+def delete(request, upload_id):
+    get_object_or_404(Upload, pk=int(upload_id)).delete()
+    return redirect('ping.views.index')
+
+
 def ping(request, size):
     size = int(size)
     assert size > 0
     return HttpResponse('a' * size)
+
 
 @csrf_exempt
 def upload(request, name):
